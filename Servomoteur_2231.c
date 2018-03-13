413c8d6eb9129f2558a7b0717689da28c50f9d20
Blazejewski09 <37115448+Blazejewski09@users.noreply.github.com>
2018-03-13 10:16:50 +0100

    A compléter

--- Servomoteur_2231.c
+++ Servomoteur_2231.c
@@ -1,18 +1,20 @@
 #include <msp430.h> 
-#include "Servomoteur_2231.h"
+//#include <Servomoteur_2231.h>
 
 
 //**********************Rotation Servomoteur*********************************
 
-//génération du signal PWM pour f=50Hz
-void Init_Timer(int Vr){
-	BCSCTL1 = CALBC1_1MHZ; //horloge 1Mhz
-	DCOCTL = CALDCO_1MHZ;
-	TACTL |= (TASSEL_2 | ID_0 | MC_1);
-	TACCTL1 |= OUTMOD_7;
-	TACCR0 = 20000;   // Correspond a 20ms
-	TACCR1 = Vr;     // Permet de gérer le rapport cyclique
-	//TACTL &= ~TAIFG;
+//gÃ©nÃ©ration du signal PWM pour f=50Hz
+void Init_Clock(){
+	 BCSCTL1 = CALBC1_1MHZ; //horloge 1Mhz
+	    DCOCTL = CALDCO_1MHZ;
+}
+void Init_Timer(){
+
+    TACTL |= (TASSEL_2 | ID_0 | MC_1);
+    TACCTL1 |= OUTMOD_7;
+    TACCR0 = 19550;   // Correspond a 20ms
+    TACTL &= ~TAIFG;
 }
 void Init_Servo(){
 
@@ -23,26 +25,37 @@ P1SEL |=BIT2;
 }
 // A utiliser pour stopper le signal
 void Stop(){
-	P1DIR |= BIT2;
-	P1SEL &= ~BIT2;
+    P1DIR |= BIT2;
+    P1SEL &= ~BIT2;
 
 }
-
+// rotation jusqu'a l'angle 90Â°
+void Droit_90(){
+	TACCR1 = 1465;
+}
+// rotation jusqu'a l'angle 0Â°
+void Gauche_0(){
+	TACCR1 = 500;
+}
+// rotation jusqu'a l'angle 180Â°
+void Droit_180(){
+	TACCR1=2500;
+}
 int main(void) {
 
-	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	Init_Servo();
-    while(1){                       // boucle permettant de répéter deux balayages
-	    Init_Timer(500);           // Position 0°
-		__delay_cycles(525000);
-		Init_Timer(1500);        // Position 90°
-		__delay_cycles(1550000);
-		Init_Timer(2500);              // Position 180°
-		__delay_cycles(525000);
-		 Init_Timer(500);
-		__delay_cycles(435000);        // Retour a la position 0°
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
 
 
-	}
+    Init_Servo();
+    Init_Clock();
+    Init_Timer();
+    Droit_90();                   //Faire une boucle avec balayage en utilisant les 3 fonctions ci-dessus
+      while(1);
+
+
+
+
+
 
 }
+
