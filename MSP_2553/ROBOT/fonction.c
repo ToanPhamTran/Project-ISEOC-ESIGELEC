f92fe435483626c3a98b0a4d236d54684f242060
Vixxxi <tphamtran@hotmail.fr>
2018-03-20 09:05:43 +0100

    Update fonction.c

--- MSP_2553/ROBOT/fonction.c
+++ MSP_2553/ROBOT/fonction.c
@@ -1,106 +1,76 @@
-#include <msp430.h>
-#include "config.h"
-#include "ADC.h"
+#include <msp430.h> 
+#include "uart.h"
+#include "fct_robot.h"
+#include <string.h>
+#include <stdio.h>
 
-
-
-
-void init_Timer()
+/*
+ * main.c
+ * test for mode manu
+ */
+void Fonction_manuel()
 {
-
-	TA1CCR0 |= 1000;
-	TA1CCTL2 |= OUTMOD_7;
-	TA1CCR2=0;
-	TA1CTL |= TASSEL_2 + MC_1;
-
-	TA1CCR0 |= 1000;
-	TA1CCTL1 |= OUTMOD_7;
-	TA1CCR1=0; 
-	TA1CTL |= TASSEL_2 + MC_1;
-
-	TA1CCR1 = 100;
-	TA1CCR2 = 100;
+    Init_Port();
+    Init_PWM();
+	Init_UART();
+	Arret_robot();
 
 
+	__enable_interrupt();
+	while(1);
 }
-
-
-void initPort()
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR()
 {
-	//Mesure vitesse moteur A bit 0
-	P2DIR &=~BIT0;
-	P2SEL |=BIT0;
-	P2SEL2 &=~BIT0;
-
-	//sens moteur A sur le bit 1
-	P2DIR |=BIT1;
-	P2SEL &=~ BIT1;
-	P2SEL2 &=~ BIT1;
-	//bit2
-	P2DIR |= BIT2;
-	P2SEL |= BIT2;
-	//Mesure vitesse moteur B bit 3
-	P2DIR &=~BIT3;
-	P2SEL |=BIT3;
-	P2SEL2 &=~BIT3;
-
-	//bit4
-	P2DIR |= BIT4;
-	P2SEL |= BIT4;
-	//sens moteur B sur le bit 5
-	P2DIR  |= BIT5;
-	P2SEL &=~ BIT5;
-	P2SEL2 &=~ BIT5;
-}
-
-
-
-void fonctionRobot()
-{
-	/*
-	 * Initialisation des variables
-	 *  */
-		int capt;
-		int tension;
-		int vitesseA;
-		int vitesseB;
-
-	_delay_cycles(2000000);
-	initPort();
-	init_Timer();
-	init_capteur();
-
-	do
+	unsigned char choix;
+	char car [100];
+	sprintf(car,"Bienvenue dans le mode manuel du Robot: %s","\r\n");
+	afficher_caract(car, strlen(car));
+	if (IFG2 & UCA0RXIFG)
 	{
-		vitesseA=192;
-		vitesseB=200;
-		Avancer_robot(vitesseA,vitesseB);
+		 choix = RXdata();
+		 sprintf(car,"Veuillez entrez votre choix : %s","\r\n");
+		 afficher_caract(car, strlen(car));
+		 sprintf(car,"1. Avancer le robot, veuillez saisir a %s","\r\n");
+		 afficher_caract(car, strlen(car));
+		 sprintf(car,"2. Tourner à droite le robot, veuillez saisir d %s","\r\n");
+		 afficher_caract(car, strlen(car));
+		 sprintf(car,"3. Tourner à gauche le robot, veuillez saisir g %s","\r\n");
+		 afficher_caract(car, strlen(car));
+		 sprintf(car,"4. Reculer le robot, veuillez saisir r %s","\r\n");
+		 afficher_caract(car, strlen(car));
+		 sprintf(car,"5. Stoper le robot, veuillez saisir s %s","\r\n");
+		 afficher_caract(car, strlen(car));
+		 switch(choix)
+		 {
+		 	 case 'a':
+		 	 	 {
+		 	 		 Avancer_robot();
+		 	 	 }
+		 	 	 break;
 
-	capt=detection_obstacle(5); //regarde valeur capteur
-	tension=detection_distance(5);
-
-	}while(tension!=327);
-
-	if(tension == 327)//tension pour une distance à 8cm
-		{
-		Arret_robot();
-		_delay_cycles(400000);
-		}
-
-		do
-		{
-			vitesseA=192;
-			vitesseB=200;
-			Avancer_robot(vitesseA,vitesseB);
-			capt=detection_obstacle(5); //regarde valeur capteur
-			tension=detection_distance(5);
-		}while(tension!=614);
-
-	 if(tension == 614) //tension pour une distance à 4cm
-			{
-				Arret_robot();
-				TA1CCR0 = 0;
-				_delay_cycles(400000);
-			}
-
+		 	 case 'd':
+		 	 	 {
+		 	 		 Tourner_droite();
+		 	 	 }
+		 	 	 break;
+		 	 case 'g':
+		 	 	 {
+		 	 		 Tourner_gauche();
+		 	 	 }
+		 	 	 break;
+		 	 case 'r':
+		 	 	 {
+		 	 		 Reculer_robot();
+		 	 	 }
+		 	 	 break;
+		 	 case 's':
+		 	 	 {
+		 	 		 Arret_robot();
+		 	 	 }
+		 	 	 break;
+		 	 default :
+		 		 break;
+		 }
+	}
 }
