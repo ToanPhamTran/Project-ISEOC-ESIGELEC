b26b06c3d19e7fe4613e62de04bf5ffb2f41e0b6
Vixxxi <tphamtran@hotmail.fr>
2018-03-20 09:06:45 +0100

    Update and rename fonction.h to fct_manuel.h

---
+++ MSP_2553/ROBOT/fct_manuel.h
@@ -0,0 +1,11 @@
+/*
+ * fct_manuel.h
+ *
+ *  Created on: 16 mars 2018
+ *      Author: t.phamtran.14
+ */
+
+void Fonction_manuel(void);
+
+
+
--- MSP_2553/ROBOT/fonction.h
+++
@@ -1,6 +0,0 @@
-/* librairy program robots
- *
- */
-void init_Timer(void);
-void initPort(void);
-void fonctionRobot(void);
