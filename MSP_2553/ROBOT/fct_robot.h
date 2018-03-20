51d23fa3a5a72db7305170f21eda6cafbb4a391a
Vixxxi <tphamtran@hotmail.fr>
2018-03-20 09:05:05 +0100

    Update and rename config.h to fct_robot.h

---
+++ MSP_2553/ROBOT/fct_robot.h
@@ -0,0 +1,55 @@
+/* librairy program robots
+ * function forward and stop
+ */
+#include <msp430.h>
+// Description : Fonction permettant d'initialiser du timer 0
+//Inputs :
+//        - void
+//Outputs :
+//         -void
+//***********************************************************************************************
+
+void Init_Timer0(void);
+
+// Description : Fonction permettant d'initialiser du timer de la vitesse du robot
+//Inputs :
+//        - void
+//Outputs :
+//         -void
+//***********************************************************************************************
+
+void Init_PWM(void);
+
+// Description : Fonction permettant d'initialiser les ports utilisés pour le capteur IR et initatilise les registres de l'ADC
+
+//Inputs :
+//        - void
+//Outputs :
+//         -void
+//***********************************************************************************************
+
+void Init_Port(void);
+
+void Init_capteur(void);
+
+void Attente_Timer0(void);
+
+void Sens_moteurA(int sens);
+
+void Sens_moteurB(int sens);
+
+void Avancer_robot();
+
+void Tourner_gauche(void);
+
+void Tourner_droite(void);
+
+void Reculer_robot(void);
+
+void Arret_robot(void);
+
+int detection_obstacle();
+
+int detection_distance();
+
+
