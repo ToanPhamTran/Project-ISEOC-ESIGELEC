df29466cac5e80e9be50e456a38105a9a9f747bd
Vixxxi <tphamtran@hotmail.fr>
2018-03-22 13:35:22 +0100

    Update main_2553.c

--- MSP_2553/main_2553.c
+++ MSP_2553/main_2553.c
@@ -4,19 +4,22 @@
 #include <string.h>
 #include <stdio.h>
 #include "Mode_auto.h"
-//
-// main.c
+//**********************************
+// main.c 
+//************************************
 void main()
 {
 
+	
 	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	BCSCTL1 = CALBC1_1MHZ;      // Set DCO to 1Mhz
 	DCOCTL = CALDCO_1MHZ;       // Set DCO to 1Mhz
-    Init_Port();
-    Init_PWM();
+	//**********************************
+	// Initialisation des registres et des ports pour l'UART & pour le robot
+	//************************************
+        Init_Port();
+        Init_PWM();
 	Init_UART();
-	Arret_robot();
-
 
 	__enable_interrupt();
 	while(1);
@@ -24,13 +27,14 @@ void main()
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR_Manuel()
 {
-	unsigned char choix;
-	char car [100];
+	unsigned char choix; // variable permettant de stocker le choix choisi par l'utilisateur
+	char car [100]; //variable permettant de stocker les caractères saisis par l'utilisateur
 	sprintf(car,"Bienvenue dans le mode manuel du Robot: %s","\r\n");
 	afficher_caract(car, strlen(car));
-	if (IFG2 & UCA0RXIFG)
+	if (IFG2 & UCA0RXIFG) // verifie si le buffer UCA0RXIFG est prêt
 	{
-		 choix = RXdata();
+		 choix = RXdata(); //le choix choisi par l'utilisateur est stocké dans la varibale "choix"
+		 //l'utilisateur choisi les différentes fonction du mode manuel ou le mode autonome
 		 sprintf(car,"Veuillez entrez votre choix : %s","\r\n");
 		 afficher_caract(car, strlen(car));
 		 sprintf(car,"1. Avancer le robot en mode manuel, veuillez saisir a %s","\r\n");
@@ -47,32 +51,37 @@ __interrupt void USCI0RX_ISR_Manuel()
 		 afficher_caract(car, strlen(car));
 		 switch(choix)
 		 {
+			//Fonction avancer du robot du mode manuel
 		 	 case 'a':
 		 	 	 {
 		 	 		 Avancer_robot();
 		 	 	 }
 		 	 	 break;
-
+			//Fonction  tourner à droite du robot en mode manuel
 		 	 case 'd':
 		 	 	 {
 		 	 		 Tourner_droite();
 		 	 	 }
 		 	 	 break;
+			//Fonction tourner à gauche du robot en mode manuel
 		 	 case 'g':
 		 	 	 {
 		 	 		 Tourner_gauche();
 		 	 	 }
 		 	 	 break;
+			//Fonction reculer du robot en mode manuel
 		 	 case 'r':
 		 	 	 {
 		 	 		 Reculer_robot();
 		 	 	 }
 		 	 	 break;
+			//Fonction Arret du robot en mode manuel
 		 	 case 's':
 		 	 	 {
 		 	 		 Arret_robot();
 		 	 	 }
 		 	 	 break;
+			//selection du mode autonome du robot
 		 	 case 'm':
 		 	 {
 		 		 Mode_auto();
