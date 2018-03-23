4bb90e4fb232360bac1f2fe39bfc2a6c190e631e
Vixxxi <tphamtran@hotmail.fr>
2018-03-23 10:07:05 +0100

    Update fct_manuel.h

--- MSP_2553/ROBOT/fct_manuel.h
+++ MSP_2553/ROBOT/fct_manuel.h
@@ -4,8 +4,16 @@
  *  Created on: 16 mars 2018
  *      Author: t.phamtran.14
  */
-
+#ifndef MANU
+#define MANU
+// Description : Fonction pour le mode manuel du robot
+//Inputs :
+//        - none
+//Outputs :
+//         -none
+//****************************************************************************
 void Fonction_manuel(void);
 
+#endif
 
 
